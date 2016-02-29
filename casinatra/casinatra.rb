7007d353d9da0b24f3c2539daafe2939e0f0ce87
ottjw <ottjw@yahoo.com>
2016-02-29 19:16:05 +0000

    changed sum stuff

--- casinatra/casinatra.rb
+++ casinatra/casinatra.rb
@@ -6,10 +6,24 @@ get '/' do
   erb :player_menu
 end
 
-get '/casino_main' do
-  @player = params['name']
+get '/casino_main' do 
+  class Player
+    attr_accessor :name, :amount
+	  def initialize(new_name)
+     @name = new_name 
+     wallet
+    end 
+
+    def wallet
+    	@amount = rand(100..1000) 
+    end
+  end 
+
+   @new_player = Player.new(params['name'])
+  
   erb :casino_main
-end
+end  
+  
 
 get '/menu_choice' do
   @choice = params[:menu_choice]
