1cbcf8a9f3d9e0fba2a84bd504dfc6c8657e2ca8
GavMan1995 <gavyncaldwell@gmail.com>
2016-02-29 11:34:36 -0700

    trying to do some stuff'

--- casinatra/casinatra.rb
+++ casinatra/casinatra.rb
@@ -2,18 +2,32 @@ require 'sinatra'
 require 'pry'
 
 
-get '/' do 
- 
-  erb :get_name
+get '/' do
+  erb :player_menu
 end
 
 get '/casino_main' do
   @player = params['name']
-  @list = ['Check Wallet', 'Hi-Lo', 'Slots', 'Black-Jack', 'New Player', 'Player Menu']
   erb :casino_main
 end
 
 get '/menu_choice' do
   @choice = params[:menu_choice]
-  erb :menu_choice 
+  erb :menu_choice
+end
+
+get '/player_menu' do
+  erb :player_menu
+end
+
+get '/hi_lo' do
+  erb :hi_lo
+end
+
+get '/slots' do
+  erb :slots
+end
+
+get '/black_jack' do
+ erb :black_jack
 end
